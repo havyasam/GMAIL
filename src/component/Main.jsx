f54c63bd39025b085cb64bcc91dd3f5903d41901
havyasam <havyasagaltha@gmail.com>
2024-01-15 22:43:27 +0530

    made some changes in sidebar

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -14,11 +14,13 @@ const Main=()=>{
         <div className="main-flex">
         <img className="mail-image"src={gmail} alt="" width="40px" height="40px" />
             <div className='mail'><p className='font-mail'>Gmail</p></div>
+            < div><img className="search-image"src={search} alt="" width="20px" height="25px" /></div>
             <div className='search'>
+            
                 <form class="example" action="/action_page.php">
-                <img className="search-image"src={search} alt="" width="20px" height="25px" />
+              
                 <img className="slider-image"src={settingslider} alt="" width="20px"/>
-                <input className='input-box' type="text" placeholder="       Search mails..." name="" />
+                <input className='input-box' type="text" placeholder="Search mails..." name="" />
                 
                 
                 </form>
@@ -28,6 +30,9 @@ const Main=()=>{
             <div className='icon-padding setting'><img src={settings} alt="" /></div>
             <div className='icon-padding account'><img src={apps} alt="" /></div>
             <div className='icon-padding sign'>Sign</div>
+        </div>
+        <div className='sidebar'> helo
+
         </div>
 
         </>
